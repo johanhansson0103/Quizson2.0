7742c38d7e1bfb6a7bc840caadf277635435c58c
eliinasvensson <elina.svensson@iths.se>
2025-10-31 12:02:14 +0100

    randomisering av svarsalternativ (specifika frågor)

--- Quizson/Fraga.java
+++ Quizson/Fraga.java
@@ -1,20 +1,43 @@
 package Quizson;
 
-public class Fraga {
+import java.util.ArrayList;
+import java.util.Collections;
+
+    public class Fraga {
     String fraga;
     String[] alternativ;
     int rattSvar;
 
-    public Fraga(String fraga, String[] alternativ, int rattSvar){
+        public Fraga(String fraga, String[] alternativ, int rattSvar){
         this.fraga = fraga;
         this.alternativ = alternativ;
         this.rattSvar = rattSvar;
+
+        blandaAlternativ();
+    }
+
+        private void blandaAlternativ(){
+        ArrayList<String> lista = new ArrayList<>();
+        Collections.addAll(lista, alternativ);
+
+        String korrektAlternativ = alternativ[rattSvar - 1];
+
+        Collections.shuffle(lista);
+
+        alternativ = lista.toArray(new String[0]);
+
+        for (int i = 0; i < alternativ.length; i++) {
+            if (alternativ[i].equals(korrektAlternativ)) {
+                rattSvar = i + 1;
+                break;
+            }
+        }
     }
 
     public void visaFraga(){
         System.out.println(fraga);
-        for (String alt : alternativ){
-            System.out.println(alt);
+        for (int i = 0; i < alternativ.length; i++) {
+            System.out.println((i + 1) + ". " + alternativ[i]);
         }
     }
 }
--- Quizson/quizMetoder.java
+++ Quizson/quizMetoder.java
@@ -1,22 +1,21 @@
 package Quizson;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class QuizMetoder{
 
-    public static void fragorOchSvar(Scanner scanner,String [] fragor, String [][] svarsAlternativ, int []rattSvar){
+    public static void fragorOchSvar(Scanner scanner, ArrayList<Fraga> fragaLista){
         int antalRatt = 0;
         long startTidFraga;
         long totalTidFragaMillis = 0;
         long svarsTid = 0;
         long startTid = System.currentTimeMillis();
-        for (int i = 0; i < fragor.length; i++) {
-
+        for (int i = 0; i < fragaLista.size(); i++) {
+            Fraga f = fragaLista.get(i);
             startTidFraga = System.currentTimeMillis();
-            System.out.println("Fråga " + (i + 1) + ": " + fragor[i]);
-
-            for (String alt : svarsAlternativ[i]) {
-                System.out.println(alt);
-            }
+            
+            System.out.println("Fråga " + (i + 1) + ": ");
+            f.visaFraga();
 
             int svar = ValideraInput.läsHeltalIInterval(scanner, 1, 4, "Ditt svar (1-4): ");
 
@@ -27,20 +26,20 @@ public class QuizMetoder{
             if (svarsTid > 30000) {
                 System.out.println("Du tog för lång tid! Max svarstid är 30 sekunder.");
                 System.out.println();
-            } else if (svar == rattSvar[i]) {
+            } else if (svar == f.rattSvar) {
                 System.out.println("Rätt svar!");
                 System.out.println();
                 antalRatt++;
                 totalTidFragaMillis += svarsTid;
             } else {
-                System.out.println("Fel svar! Rätt svar var: " + svarsAlternativ[i][rattSvar[i] - 1]);
+                System.out.println("Fel svar! Rätt svar var: " + f.alternativ[f.rattSvar - 1]);
                 System.out.println();
             } 
         }
         
         long slutTid = System.currentTimeMillis();
 
-        System.out.println("Du fick " + antalRatt + " av " + fragor.length + " rätt!");
+        System.out.println("Du fick " + antalRatt + " av " + fragaLista.size() + " rätt!");
         TidOchPoang.tidPoang(slutTid, startTid, totalTidFragaMillis, antalRatt);
     }
 }
